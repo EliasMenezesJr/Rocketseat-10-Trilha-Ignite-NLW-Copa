23a028726e8580107aaeffe61cb5a509983908f9
Elias Menezes Júnior <eliasmenezes2510@gmail.com>
2023-04-04 18:55:50 -0300

    create web

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,22 @@
+interface HomeProps {
+  count: number
+}
+
+
+
+export function Home(props:HomeProps) {
+  return (
+  <h1>{props.count}</h1>
+  )
+}
+
+export const getServerSiderProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count')
+
+  const data = await response.json()
+
+  console.log(data);
+  return {
+    props : {count: data.count}
+  }
+}
